6081ce3ba14565e88c6a59aa1bf147481014fb82
kaiqueZpriv1 <kakamelo141205@gmail.com>
2023-03-09 18:28:02 -0300

    main finish

--- assets/scripts/header/script.js
+++ assets/scripts/header/script.js
@@ -1,28 +1,52 @@
 const body = document.createElement("body");
 const header = document.createElement("section");
-const textDivHeader = document.createElement("div")
-const textoHeader = document.createElement("h3");
-const subHeader = document.createElement("p")
-const spanHeader = document.createElement("span");
-const menu = document.createElement("img");
-const iconMusculacao = document.createElement("img")
+let textDivHeader = document.createElement("div");
+let textoHeader = document.createElement("h3");
+let subHeader = document.createElement("p");
+let spanHeader = document.createElement("span");
+let menu = document.createElement("img");
+let iconMusculacao = document.createElement("img");
 
 header.setAttribute("id", "Header");
 textoHeader.textContent = "Personal ";
-subHeader.textContent = "Especialista em entregar resultados"
+subHeader.textContent = "Especialista em entregar resultados";
 spanHeader.textContent = "Trainer";
-menu.src = "./assets/img/imgHeader/menu-red.svg"
-iconMusculacao.src = "./assets/img/imgHeader/musculacao.png"
+menu.src = "./assets/img/imgHeader/menu-red.svg";
+iconMusculacao.src = "./assets/img/imgHeader/musculacao.png";
 
 // header.appendChild(logo);
-header.appendChild(textDivHeader)
-textDivHeader.appendChild(textoHeader)
-textDivHeader.appendChild(subHeader)
+header.appendChild(textDivHeader);
+textDivHeader.appendChild(textoHeader);
+textDivHeader.appendChild(subHeader);
 textoHeader.appendChild(spanHeader);
-header.appendChild(iconMusculacao)
+header.appendChild(iconMusculacao);
 header.appendChild(menu);
 body.appendChild(header);
 document.documentElement.appendChild(body);
 
+// CREATE MAIN
+const main = document.createElement("section");
+const containerMain = document.createElement("section");
+const DivTextMain = document.createElement("div");
+let h1Main = document.createElement("h1");
+let subMain = document.createElement("p");
+// ATRIBUTOS
+main.setAttribute("id", "main");
+containerMain.setAttribute("id", "conteiner-main");
+h1Main.setAttribute("class", "h1Main");
 
+// DECLARACAO DE CONTEUDO
+h1Main.textContent = "Juan Bueno";
+subMain.textContent = "Conquiste a qualidade de vida que sempre sonhou";
 
+// APPEND CHILD
+//textos
+DivTextMain.appendChild(h1Main);
+DivTextMain.appendChild(subMain);
+containerMain.appendChild(DivTextMain);
+// containerMain.appendChild(divLinksMain);
+main.appendChild(containerMain);
+body.appendChild(main);
+
+// conteudo
+const conteudo = document.createElement("section");
--- assets/scripts/header/style.js
+++ assets/scripts/header/style.js
@@ -3,22 +3,44 @@ header.style.height = "75px";
 header.style.display = "flex";
 
 textoHeader.style.color = "#FAFAFA";
-textoHeader.style.fontSize = "4vw";
+textoHeader.style.fontSize = "3vw";
 textoHeader.style.fontFamily = "Source code pro";
 textoHeader.style.position = "relative";
-textoHeader.style.top = "5px";
+textoHeader.style.top = "20px";
 textoHeader.style.left = "25px";
 
 subHeader.style.color = "#FAFAFA";
 subHeader.style.fontFamily = "Roboto slab";
 subHeader.style.marginLeft = "10px";
+subHeader.style.marginTop = "15px";
+subHeader.style.fontSize = "2vw";
 
 menu.style.width = "45px";
 menu.style.position = "relative";
 menu.style.top = "0px";
-menu.style.left = "275px";
+menu.style.left = "300px";
 
-iconMusculacao.style.position = "absolute"
-iconMusculacao.style.width = "45px"
-iconMusculacao.style.top = "10px"
-iconMusculacao.style.left = "280px"
+iconMusculacao.style.position = "absolute";
+iconMusculacao.style.width = "45px";
+iconMusculacao.style.top = "10px";
+iconMusculacao.style.left = "210px";
+
+// main
+main.style.height = "70vw";
+DivTextMain.style.margin = "150px";
+DivTextMain.style.textAlign = "center";
+
+h1Main.style.fontSize = "7vw";
+h1Main.style.fontFamily = "Roboto slab";
+h1Main.style.fontWeight = "900";
+h1Main.style.color = "#FAFAFA";
+h1Main.style.textShadow = "0 0 5px #fff";
+
+subMain.style.fontSize = "5vw";
+subMain.style.position = "absolute";
+subMain.style.right = "10px";
+subMain.style.top = "300px";
+subMain.style.color = "#FDFDFD";
+subMain.style.textShadow = "0 0 20px #000";
+subMain.style.fontFamily = "Roboto";
+subMain.style.fontWeight = "900";
