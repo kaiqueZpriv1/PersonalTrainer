c52c67faffb47db639f407bcdfee3fed733ad061
kaiqueZpriv1 <kakamelo141205@gmail.com>
2023-03-09 23:01:38 -0300

    alt

--- assets/scripts/header/style.js
+++ assets/scripts/header/style.js
@@ -18,12 +18,12 @@ subHeader.style.fontSize = "2vw";
 menu.style.width = "45px";
 menu.style.position = "relative";
 menu.style.top = "0px";
-menu.style.left = "300px";
+menu.style.left = "270px";
 
 iconMusculacao.style.position = "absolute";
 iconMusculacao.style.width = "45px";
 iconMusculacao.style.top = "10px";
-iconMusculacao.style.left = "210px";
+iconMusculacao.style.left = "180px";
 
 // main
 main.style.height = "70vw";
@@ -35,11 +35,12 @@ h1Main.style.fontFamily = "Roboto slab";
 h1Main.style.fontWeight = "900";
 h1Main.style.color = "#FAFAFA";
 h1Main.style.textShadow = "0 0 5px #fff";
+h1Main.style.position = "absolute";
 
 subMain.style.fontSize = "5vw";
 subMain.style.position = "absolute";
 subMain.style.right = "10px";
-subMain.style.top = "300px";
+subMain.style.top = "320px";
 subMain.style.color = "#FDFDFD";
 subMain.style.textShadow = "0 0 20px #000";
 subMain.style.fontFamily = "Roboto";
